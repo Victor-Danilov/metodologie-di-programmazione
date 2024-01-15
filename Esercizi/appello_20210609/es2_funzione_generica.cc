6738bde612aedd434f10494545752c503a074416
Victor Danilov <victordanilov@outlook.it>
2024-01-15 11:50:23 +0100

    correzioni #1

--- Esercizi/appello_20210609/es2_funzione_generica.cc
+++ Esercizi/appello_20210609/es2_funzione_generica.cc
@@ -1,3 +1,7 @@
 /*
     Funzione generica
+
+    Definire la funzione generica none_of che prende in input una sequenza ed un predicato unario
+    e restituisce in output un booleano: la funzione restiruisce il valore true se e solo se nessuno
+    degli elementi della sequenza soddisfano il predicato.
 */

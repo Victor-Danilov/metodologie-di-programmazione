e39b9c689e8d85dbee854ed90dd1690830d0ccae
Victor Danilov <victordanilov@outlook.it>
2024-01-21 18:09:05 +0100

    correzioni

--- Esercizi/appello_20120202/es2_funzione_generica.cc
+++ Esercizi/appello_20120202/es2_funzione_generica.cc
@@ -11,7 +11,7 @@
 
 template<typename Iter, typename Pred>
 typename std::iterator_traits<Iter>::difference_type
-    count_if(Iter first, Iter last, Pred pred)
+    count_if(Iter first, Iter last, Pred& pred)
 {
     typename std::iterator_traits<Iter>::difference_type count=0;
     for(; first!=last; ++first){
